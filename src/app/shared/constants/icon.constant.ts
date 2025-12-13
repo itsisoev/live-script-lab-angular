321c098dae2259a0c4b8e0131ff481757af81544
Daler <isoev9733@gmail.com>
2025-12-13 19:53:31 +0400

    - добавил новый путь к @shared/ - сделал форматирование

--- src/app/shared/constants/icon.constant.ts
+++ src/app/shared/constants/icon.constant.ts
@@ -1,13 +1,15 @@
 import {
-    ChevronLeft,
-    Settings,
-    CircleX
+  ChevronLeft,
+  Settings,
+  CircleX,
+  Play
 } from 'lucide-angular';
 
 export const ICONS = {
-    ChevronLeft,
-    Settings,
-    CircleX
+  ChevronLeft,
+  Settings,
+  CircleX,
+  Play
 } as const;
 
 export type IconName = keyof typeof ICONS;
