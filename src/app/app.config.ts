80029bc9ab9a65d42e33518fdfd5a732f9c9394c
Daler <isoev9733@gmail.com>
2025-12-13 22:18:34 +0400

    - добавил в provideRouter: withHashLocation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import {provideRouter, withHashLocation} from '@angular/router';
 
 import {routes} from './app.routes';
 import {ChevronLeft, LUCIDE_ICONS, LucideIconProvider} from 'lucide-angular';
@@ -8,7 +8,7 @@ import {ICONS} from './shared/constants/icon.constant';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     {provide: LUCIDE_ICONS, multi: true, useValue: new LucideIconProvider(ICONS)},
   ]
 };
